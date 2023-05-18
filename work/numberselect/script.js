ac716385f451e8c9599c94466c3369b2863438cb
sueasen <suehara.akihiko@gmail.com>
2023-05-19 02:28:46 +0900

    ハード部分まとめる

--- work/numberselect/script.js
+++ work/numberselect/script.js
@@ -1,44 +1,51 @@
-const numTarget = Math.floor(Math.random() * 90);
-document.querySelector('#main').innerText = '？？？';
-const numbers = [...Array(9 * 10).keys()];
+const boxCount = 90;
+const numbers = {
+    target: null
+    , list: null
+};
 
 let count = 0;
 let select = null;
-while (numbers.length) {
-    if (count++ % 9 == 0) {
+for (let index = 0; index < boxCount; index++) {
+    if (index % (boxCount / 10) == 0) {
         select = document.createElement('div');
         select.classList.add('flex_center');
         document.querySelector('.select').append(select);
     }
-    let i = Math.floor(Math.random() * numbers.length);
-    let num = numbers.splice(i, 1);
-
     const box = document.createElement('div');
     box.classList.add('box');
-    box.innerText = num;
     select.append(box);
     box.addEventListener('click', () => isNumber(box));
 }
+setNumbers();
+document.querySelector('#main').addEventListener('click', setNumbers)
 
-document.querySelector('#odd').addEventListener('click', () => {
-    document.querySelector('.select').querySelectorAll('.box').forEach(v => {
-        if (v.innerText % 2 != numTarget % 2) {
+function setNumbers() {
+    numbers.target = Math.floor(Math.random() * boxCount);
+    numbers.list = [...Array(boxCount).keys()];
+    document.querySelectorAll('.select .box').forEach(v => {
+        let i = Math.floor(Math.random() * numbers.list.length);
+        let num = numbers.list.splice(i, 1);
+        v.innerText = num;
+        v.style.opacity = 1;
+    });
+    document.querySelector('#odd').addEventListener('click', () => addEventHint((t) => t % 2 != numbers.target % 2));
+    document.querySelector('#herf').addEventListener('click', () => addEventHint((t) => parseInt(t / (boxCount / 2)) != parseInt(numbers.target / (boxCount / 2))));
+    document.querySelector('#main').innerText = '？？？';
+}
+
+function addEventHint(isIgnore) {
+    document.querySelectorAll('.select .box').forEach(v => {
+        if (isIgnore(v.innerText)) {
             v.style.opacity = 0;
         }
     });
-});
-document.querySelector('#herf').addEventListener('click', () => {
-    document.querySelector('.select').querySelectorAll('.box').forEach(v => {
-        if (parseInt(v.innerText / 45) != parseInt(numTarget / 45)) {
-            v.style.opacity = 0;
-        }
-    });
-});
+}
 
 function isNumber(box) {
-    if (box.innerText == numTarget) {
-        document.querySelector('#main').innerText = '正解！！';
-    } else if (box.innerText > numTarget){
+    if (box.innerText == numbers.target) {
+        document.querySelector('#main').innerText = '正解！！またやる？';
+    } else if (box.innerText > numbers.target) {
         document.querySelector('#main').innerText = `？ ${box.innerText}より小さいよ ？`;
     } else {
         document.querySelector('#main').innerText = `？ ${box.innerText}より大きいよ ？`;

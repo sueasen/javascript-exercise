261ac33c54229ebd3f326d0cc9f76492211c7ea4
sueasen <suehara.akihiko@gmail.com>
2023-05-19 12:50:06 +0900

    目押し対応

--- work/janken/script.js
+++ work/janken/script.js
@@ -11,8 +11,15 @@ function playJanken() {
 
 function selectHand(hand) {
 
+    // pcHand を乱数使用して設定
+    // document.getElementById('pc-hand').classList.remove('rand');
+    // let pcHand = janken[parseInt(Math.random() * janken.length)];
+
+    //　pcHand アニメーションに合わせて設定 
+    let pcHandNowImage = getComputedStyle(document.getElementById("pc-hand")).backgroundImage.split(',')[1];
+    let pcHand = pcHandNowImage.substring(pcHandNowImage.indexOf('.png') - 4, pcHandNowImage.indexOf('.png'));
     document.getElementById('pc-hand').classList.remove('rand');
-    let pcHand = janken[parseInt(Math.random() * janken.length)];
+    
     document.getElementById('pc-hand').style.backgroundImage = `url(./${pcHand}.png)`;
 
     if (pcHand == hand) {

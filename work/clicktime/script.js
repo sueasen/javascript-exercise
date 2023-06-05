3b5e056cabbc00747e7bd314f507096d1565ecb2
sueasen <suehara.akihiko@gmail.com>
2023-06-05 11:16:55 +0900

    開始制御修正

--- work/clicktime/script.js
+++ work/clicktime/script.js
@@ -26,6 +26,7 @@ function startGame() {
     isGameStart = true;
     document.querySelector('#target').style.display = 'block';
     moveTarget();
+    clearInterval(intervalId);
     intervalId = setInterval(() => {
         document.querySelector('#result').innerText = `${progressTime()} 秒経過．．．`;
     }, 50);

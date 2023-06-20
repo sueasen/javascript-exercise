a44e430cb832e1a6b8972300bc4331c6edb5073c
sueasen <suehara.akihiko@gmail.com>
2023-06-21 06:23:11 +0900

    録画追加

--- work/camera/script.js
+++ work/camera/script.js
@@ -1,18 +1,24 @@
 const video = document.querySelector('#video');
 const canvas = document.createElement('canvas');
+let mediaRecorder;
+const movie = document.querySelector('#movie');
 
 initVideoCamera();
 initPhoto();
 document.querySelector('#shoot').addEventListener('click', photoShoot);
+document.querySelector('#recode-start').addEventListener('click', recodeStart);
+document.querySelector('#recode-stop').addEventListener('click', recodeStop);
 
 /**
  * ビデオのカメラ設定(デバイスのカメラ映像をビデオに描画)
  */
 function initVideoCamera() {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         .then((stream) => {
             video.srcObject = stream;
             video.play();
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.addEventListener('dataavailable', e => movie.src = URL.createObjectURL(e.data));
         })
         .catch(e => console.log(e));
 }
@@ -43,7 +49,7 @@ function photoShoot() {
 
 /**
  * 描画サイズの計算
- * 縦横比の撮影(video)が大きい時は撮影の縦基準、それ以外は撮影の横基準で計算
+ * 縦横比が撮影(video)が大きい時は撮影の縦基準、それ以外は撮影の横基準で計算
  */
 function calcDrawSize() {
     let videoRatio = video.videoHeight / video.videoWidth;
@@ -51,4 +57,22 @@ function calcDrawSize() {
     return videoRatio > viewRatio ?
         { height: video.clientHeight, width: video.clientHeight / videoRatio }
         : { height: video.clientWidth * videoRatio, width: video.clientWidth }
+}
+
+/**
+ * 動画撮影の開始
+ */
+function recodeStart() {
+    mediaRecorder.start();
+    document.querySelector('#recode-start').classList.add('hidd');
+    document.querySelector('#recode-stop').classList.remove('hidd');
+}
+
+/**
+ * 動画撮影の停止
+ */
+function recodeStop() {
+    mediaRecorder.stop();
+    document.querySelector('#recode-start').classList.remove('hidd');
+    document.querySelector('#recode-stop').classList.add('hidd');
 }

ddcf3bab02844ce129892b85d0bb80c1d15265fe
sueasen <suehara.akihiko@gmail.com>
2023-06-28 01:37:35 +0900

    まとめページ

---
+++ work/allplay/script.js
@@ -0,0 +1,20 @@
+const iframe = document.querySelector('#ifcontainer');
+
+// document.querySelector('#slide-item-1').addEventListener('click', () =>
+//     iframe.src = 'https://sueasen.github.io/javascript-exercise/work/janken/'
+// )
+document.querySelector('#slide-item-1').addEventListener('click', () =>
+    iframe.src = 'https://sueasen.github.io/javascript-exercise/work/colorselect/'
+)
+document.querySelector('#slide-item-2').addEventListener('click', () =>
+    iframe.src = 'https://sueasen.github.io/javascript-exercise/work/numberselect/'
+)
+document.querySelector('#slide-item-3').addEventListener('click', () =>
+    iframe.src = 'https://sueasen.github.io/javascript-exercise/work/allwhite/'
+)
+document.querySelector('#slide-item-4').addEventListener('click', () =>
+    iframe.src = 'https://sueasen.github.io/javascript-exercise/work/clicktime/'
+)
+document.querySelector('#slide-item-5').addEventListener('click', () =>
+    iframe.src = 'https://sueasen.github.io/javascript-exercise/work/magicsquare/'
+)
